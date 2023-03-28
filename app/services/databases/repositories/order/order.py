0842df859fd0468c2315fcb63584897faecc97d7
arsen <arsendada@gmail.com>
2023-03-28 23:08:50 +0300

    created add_order method to OrderCrud

--- app/services/databases/repositories/order/order.py
+++ app/services/databases/repositories/order/order.py
@@ -1,5 +1,15 @@
+from app.services.databases.models.order.order import Order
 from app.services.databases.repositories.base import BaseCrud
+from app.services.databases.schemas.order.order import OrderModel
 
 
 class OrderCrud(BaseCrud):
-    pass
+
+    async def add_order(self,
+                        data: OrderModel):
+
+        new_order = Order(**data.dict())
+        self.sess.add(new_order)
+        await self.sess.commit()
+        await self.sess.refresh(new_order)
+        return new_order
